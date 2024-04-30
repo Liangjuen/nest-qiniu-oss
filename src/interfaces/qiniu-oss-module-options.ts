ec6c46945ba6602f8d9949e8fabd0895f9201cc4
梁举恩 <3103421940@qq.com>
2024-04-30 17:30:00 +0800

    ✨ feat: 新增region配置

--- src/interfaces/qiniu-oss-module-options.ts
+++ src/interfaces/qiniu-oss-module-options.ts
@@ -1,6 +1,12 @@
 import { ModuleMetadata, Type } from '@nestjs/common'
 import { conf, auth } from 'qiniu'
 
+/**
+ * 存储区域
+ * @see https://developer.qiniu.com/kodo/1671/region-endpoint-fq
+ */
+export type Region = 'z0' | 'cn-east-2' | 'z1' | 'z2' | 'na0' | 'as0' | 'ap-southeast-2'
+
 export interface QiniuOSSModuleOptions {
 	/**
 	 * 用于鉴权对象 `mac` 的 `accessKey`
@@ -31,6 +37,12 @@ export interface QiniuOSSModuleOptions {
 	 * @see https://developer.qiniu.com/kodo/1671/region-endpoint-fq
 	 */
 	zone?: conf.Zone
+
+	/**
+	 * 存储区域 若不配置，将会通过 AK 与 Bucket 查询对应区域。若非必要，建议不配置区域信息
+	 * @see https://developer.qiniu.com/kodo/1671/region-endpoint-fq
+	 */
+	region?: Region
 
 	/**
 	 * 鉴权对象配置
