4ac1cd4b56f96c8617dddebaa51654f1fc25fef7
梁举恩 <3103421940@qq.com>
2024-04-30 17:31:10 +0800

    ✨ feat: 新增 createRegionsProvider

--- src/core/oss.service.ts
+++ src/core/oss.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject, Optional } from '@nestjs/common'
 import * as qiniu from 'qiniu'
 import { QINIU_OSS_MODULE_OPTIONS } from '../constants'
-import { QiniuOSSModuleOptions } from '../interfaces'
+import { QiniuOSSModuleOptions, Region } from '../interfaces'
 
 /**
  * 七牛云 OSS 服务
@@ -29,13 +29,30 @@ export class QiniuOSSService {
 	}
 
 	/**
+	 * @description 创建 `regionsProvider `
+	 * @param `regionId` https://developer.qiniu.com/kodo/1671/region-endpoint-fq
+	 * @param options
+	 * @returns
+	 */
+	createRegionsProvider(
+		regionId?: Region,
+		options?: qiniu.httpc.RegionFromRegionIdOptions
+	): qiniu.httpc.RegionsProvider {
+		const region = qiniu.httpc.Region.fromRegionId(regionId || this.options.region, options)
+		return {
+			getRegions: async () => [region]
+		}
+	}
+
+	/**
 	 * @description 创建配置
 	 * @param config 实例配置
 	 * @returns
 	 */
 	createConfig(config?: qiniu.conf.ConfigOptions) {
-		return new qiniu.conf.Config({
+		return new this.engine.conf.Config({
 			zone: this.options.zone,
+			regionsProvider: this.createRegionsProvider(),
 			...config
 		})
 	}
@@ -46,7 +63,7 @@ export class QiniuOSSService {
 	 * @returns 鉴权对象
 	 */
 	mac(): qiniu.auth.digest.Mac {
-		return new qiniu.auth.digest.Mac(
+		return new this.engine.auth.digest.Mac(
 			this.options.accessKey,
 			this.options.secretKey,
 			this.options.macOptions
@@ -59,8 +76,11 @@ export class QiniuOSSService {
 	 * @param options 配置
 	 * @returns `policy`
 	 */
-	createPutPolicy(options: qiniu.rs.PutPolicyOptions) {
-		return new qiniu.rs.PutPolicy(options)
+	createPutPolicy(options?: qiniu.rs.PutPolicyOptions) {
+		return new this.engine.rs.PutPolicy({
+			scope: this.options.bucket,
+			...options
+		})
 	}
 
 	/**
@@ -69,7 +89,7 @@ export class QiniuOSSService {
 	 * @param options [上传策略配置](https://developer.qiniu.com/kodo/1206/put-policy)
 	 * @returns
 	 */
-	createUploadToken(options: qiniu.rs.PutPolicyOptions) {
+	createUploadToken(options?: qiniu.rs.PutPolicyOptions) {
 		return this.createPutPolicy(options).uploadToken(this.mac())
 	}
 
@@ -80,7 +100,7 @@ export class QiniuOSSService {
 	 * @returns
 	 */
 	createBucketManager(config?: qiniu.conf.ConfigOptions) {
-		return new qiniu.rs.BucketManager(this.mac(), {
+		return new this.engine.rs.BucketManager(this.mac(), {
 			...this.createConfig(),
 			...config
 		})
@@ -92,6 +112,6 @@ export class QiniuOSSService {
 	 * @returns CdnManager
 	 */
 	createCdnManager(mac?: qiniu.auth.digest.Mac) {
-		return new qiniu.cdn.CdnManager(mac || this.mac())
+		return new this.engine.cdn.CdnManager(mac || this.mac())
 	}
 }
